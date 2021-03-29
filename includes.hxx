274d517014c7fe2655ac22dafc8133588ece5988
Macarthur Inbody <133794m3r@gmail.com>
2021-03-29 18:50:03 -0400

    All systems are done I think. Time to push to master and then it'll be time to fully divvy up work.

---
+++ includes.hxx
@@ -0,0 +1,14 @@
+/*
+* ITP-298 Capstone Project
+* Group: Dangling Pointers
+* Macarthur Inbody && Nathaniel Mullins
+* Created by macarthur on 3/29/21.
+*
+*/
+
+//this file will contain all of the common includes in a single header.
+#ifndef ITP298_CAPSTONE_INCLUDES_HXX
+#define ITP298_CAPSTONE_INCLUDES_HXX
+#include <iostream>
+#include <vector>
+#endif //ITP298_CAPSTONE_INCLUDES_HXX
