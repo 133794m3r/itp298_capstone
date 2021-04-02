384ac77ddbbf60324e713fd7950517e0accd6364
Macathur Inbody <133794m3r@gmail.com>
2021-04-02 13:00:57 -0400

    classes/fighters/Actor.hxx did first pass of the base class.
    Added basic properties including the initializer function and marked it explicit.

--- classes/fighters/actor.hxx
+++ classes/fighters/actor.hxx
@@ -8,10 +8,26 @@
 #ifndef ITP298_CAPSTONE_ACTOR_HXX
 #define ITP298_CAPSTONE_ACTOR_HXX
 
-
 class Actor {
-
+  private:
+	std::string _name;
+	char _lvl;
+	unsigned int _hp;
+	unsigned int _str;
+	unsigned int _def;
+  public:
+	Actor(std::string name="", unsigned int hp = 0, unsigned int str=0, unsigned int def=0){
+		this->_name = name;
+		this->_hp = hp;
+		this->_str = str;
+		this->_def = def;
+	}
+	bool damage(int dmg){
+		if(this->_hp < dmg)
+			return false;
+		this->_hp -= dmg;
+		return true;
+	}
 };
-
 
 #endif //ITP298_CAPSTONE_ACTOR_HXX
