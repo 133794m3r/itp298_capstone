fbe020de782c8153c70dc4953523f04f98446b45
Macathur Inbody <133794m3r@gmail.com>
2021-04-02 17:32:02 -0400

    Updated classes/fighters/actor.hxx to add damage and attack methods.
    The class now has an attack method and also a damage method. The damage method also takes into account the defenders defense when calculating the total damage.

--- classes/fighters/actor.hxx
+++ classes/fighters/actor.hxx
@@ -16,18 +16,38 @@ class Actor {
 	unsigned int _str;
 	unsigned int _def;
   public:
-	Actor(std::string name="", unsigned int hp = 0, unsigned int str=0, unsigned int def=0){
+	explicit Actor(std::string name="", unsigned int hp = 0, unsigned int str=0, unsigned int def=0){
 		this->_name = name;
 		this->_hp = hp;
 		this->_str = str;
 		this->_def = def;
 	}
-	bool damage(int dmg){
+	/**
+	*
+	* @parm int the amount of damage to be inflicted.
+	* @return int The total amount of damage delivered. 0 for none, and - value for overkill.
+	*/
+	int damage(unsigned int dmg){
+		dmg -= this->_def;
+		if(dmg < 0)
+			return 0;
 		if(this->_hp < dmg)
-			return false;
+			this->_hp = 0;
+			return dmg*-1;
+		}
 		this->_hp -= dmg;
-		return true;
+		return dmg;
 	}
+	/**
+	* 
+	*
+	* @param the target actor to damage. Uses this object's strength.
+	* @return int the amount of damage carried out.
+	*/
+	int attack(Actor &target){
+		return target->damage(this->_str);
+	}
+
 };
 
 #endif //ITP298_CAPSTONE_ACTOR_HXX
