38e0bf518ec0050297510c61e9d01ddf199b5d28
Nathan Mullins <stadia24216@gmail.com>
2021-04-12 21:43:39 -0500

    fixed constructor

--- classes/items/itembase.cpp
+++ classes/items/itembase.cpp
@@ -18,18 +18,18 @@ void Item::generate()
 
 
 //Constructors / Destructors
-Item(std::string name,
+Item::Item(std::string name,
 unsigned int type,
 unsigned int tier,
 unsigned int value,
-unsigned int level)
-{
-this->name = name; //assigns the given value of name
-this->type = type;  //assigns the given value of type
-this->value = value; //assigns the given value of value
-this->tier = tier; //assigns the given value of tier
-this->level = level; //assigns the given value of level
-}
+unsigned int level){
+
+this->name = ""; //assigns the given value of name
+this->type = 0;  //assigns the given value of type
+this->value = 0; //assigns the given value of value
+this->tier = 0; //assigns the given value of tier
+this->level = 0;} //assigns the given value of level
+
 
 Item::~Item()
 {
