92aa877fbfc79d60c4a6ff1e124ea07f1f3571d4
Macarthur Inbody <133794m3r@gmail.com>
2021-04-19 21:34:56 -0400

    Done for the day. ShopKeeper is making less sense as I look at it.

--- classes/containers/shop_inventory.hxx
+++ classes/containers/shop_inventory.hxx
@@ -9,10 +9,30 @@
 #ifndef ITP298_CAPSTONE_SHOP_INVENTORY_HXX
 #define ITP298_CAPSTONE_SHOP_INVENTORY_HXX
 #include "inventory.hxx"
+#include "../../includes.hxx"
 class ShopInventory: public Inventory {
   public:
-	bool purchase_item(unsigned_short item_id, unsigned int amount=1){
-		return true;
+	explicit ShopInventory(std::vecotr<){
+
+	}
+	/**
+	 * The cost to buy amount of the items.
+	 * @param item_id The item we're going to be using.
+	 * @param amount The amount to attempt to purchase
+	 * @return The amount of gold it'd cost to buy it.
+	 */
+	unsigned int purchase_cost(unsigned short item_id, unsigned int amount=1){
+		return this->items[item_id]->get_value() * amount;
+	}
+
+	/**
+	 * The value of the items to be sold. Always less than their value.
+	 * @param item_id The id of the item we're going to be selling.
+	 * @param amount
+	 * @return
+	 */
+	unsigned int sell_value(Inventory &inv, unsigned short item_id, unsigned int amount=1){
+		return std::lround(inv->purchase_cost(item_id,amount)*0.75);
 	}
 };
 
--- classes/shopkeeper.hxx
+++ classes/shopkeeper.hxx
@@ -8,10 +8,19 @@
 
 #ifndef ITP298_CAPSTONE_SHOPKEEPER_HXX
 #define ITP298_CAPSTONE_SHOPKEEPER_HXX
+#include <utility>
 
-
+#include "containers/shop_inventory.hxx"
 class ShopKeeper {
-
+  private:
+	ShopInventory shop_inventory_;
+	std::string name_;
+  public:
+	explicit ShopKeeper(std::string name = "Shop Keep", const std::vector<Item*> &items = {}, const std::vector<unsigned int> &quantity = {}){
+		this->shop_inventory_ = ShopInventory(items,quantity);
+		this->name_ = std::move(name);
+	}
+	bool purchase_item();
 };
 
 
