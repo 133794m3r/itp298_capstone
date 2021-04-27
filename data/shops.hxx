3d049252c0f7efb7efe708ca723987cddf0353c0
Macarthur Inbody <133794m3r@gmail.com>
2021-04-26 20:32:28 -0400

    Started adding the data and getting ready for the final.

---
+++ data/shops.hxx
@@ -0,0 +1,12 @@
+/*
+* ITP-298 Capstone Project
+* Group: Dangling Pointers
+* Macarthur Inbody && Nathaniel Mullins
+* Created on 4/26/21.
+*
+*/
+
+#ifndef ITP298_CAPSTONE_SHOPS_HXX
+#define ITP298_CAPSTONE_SHOPS_HXX
+
+#endif //ITP298_CAPSTONE_SHOPS_HXX
